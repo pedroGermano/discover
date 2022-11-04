65a9595d4df2cc75c772ed345f835a328ccbf39e
Pedro Germano <pedrogermano232@gmail.com>
2022-11-04 14:39:59 -0300

    callback done

--- js-promise/callback.js
+++ js-promise/callback.js
@@ -1,1 +1,12 @@
-function callback(){}
+// Funções aceitam qualquer tipo de dado com argumento
+function imprimirDado(dado){
+  console.log('outras tarefas')
+  console.log(dado())
+}
+
+// imprimirDado(1)
+// imprimirDado('texto')
+
+imprimirDado(function (){
+  return 'Olá mundo'
+})
